cda9c57564dd306fea87f5ddafa0890a1aaf997e
Meezy98 <kymon.williams@revature.net>
2022-02-14 23:11:37 -0500

    Created Withdraw Entity, as well as dependent entities of the lookup tables

--- src/main/java/com/revature/thevault/repository/entity/AccountProfileEntity.java
+++ src/main/java/com/revature/thevault/repository/entity/AccountProfileEntity.java
@@ -20,7 +20,7 @@ public class AccountProfileEntity {
     @GeneratedValue(generator = "account_profile_table_pk_profile_id_seq", strategy = GenerationType.SEQUENCE)
     @SequenceGenerator(allocationSize = 1, name = "account_profile_table_pk_profile_id_seq", sequenceName = "account_profile_table_pk_profile_id_seq")
     int pk_profile_id;
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "fk_user_id")
     LoginCredentialEntity logincredential;
     @Column(name = "first_name")
