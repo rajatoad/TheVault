8a051ffa7f1e6a57eac0545dc16d7d287d50d51a
DementedTiger <72563448+DementedTiger@users.noreply.github.com>
2022-02-21 13:46:41 -0600

    feat(css): Css has been created creating a clean layout bug(cssText): Some text are white on white background bugFix(selectComponent): Creating an accoutn was not updating the list of avaiable accounts, fixed now

--- TheVault/src/app/components/account/account-view/account-view.component.ts
+++ TheVault/src/app/components/account/account-view/account-view.component.ts
@@ -29,10 +29,15 @@ export class AccountViewComponent implements OnInit {
       this.userId = this.globalStorage.getUserId();
       this.profile = this.globalStorage.getProfile();
       this.accounts = this.globalStorage.getAccounts();
-      console.log(this.profile.firstName);
     }catch(err){
       console.error(err);
     }
   }
+
+  createAccountEvent(submit:boolean){
+    console.log(submit)
+    if(submit)this.accounts = this.globalStorage.getAccounts();
+  }
+
 
 }
