ce9a35e45f36f22a3d23d1e1ece2be54072bb303
btwomack <btwomack@gmail.com>
2022-02-22 12:27:03 -0600

    quick push

--- TheVault/src/app/_services/app_control/routing-allocator.service.spec.ts
+++ TheVault/src/app/_services/app_control/routing-allocator.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
 
@@ -7,6 +7,7 @@ import { Location, LocationStrategy } from '@angular/common';
 import { AppComponent } from 'src/app/app.component';
 
 describe('RoutingAllocatorService', () => {
+  let component = RoutingAllocatorService;
   let service: RoutingAllocatorService;
   let location: Location;
   let router: Router;
@@ -19,7 +20,21 @@ describe('RoutingAllocatorService', () => {
     service = TestBed.inject(RoutingAllocatorService);
   });
 
+  it("fakeAsync works", fakeAsync(() => {
+    let promise = new Promise(resolve => {
+      setTimeout(resolve, 10);
+    });
+    let done = false;
+    promise.then(() => (done = true));
+    tick(50);
+    expect(done).toBeTruthy();
+  }));
 
+  // it('should navigate to "" redirects to /register', fakeAsync(() => {
+  //   router.navigate(['register']);
+  //   tick();
+  //   expect(location.path()).toBe('/register')
+  // }))
 
   it('should be created', () => {
     expect(service).toBeTruthy();
