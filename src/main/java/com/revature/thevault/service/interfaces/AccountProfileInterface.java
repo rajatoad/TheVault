9a3086be78f2e04477594ac8a8f59379677b85b2
DementedTiger <72563448+DementedTiger@users.noreply.github.com>
2022-02-21 23:56:09 -0600

    feat(postmanTesting): Setup up integration testing with postman, had to rework some of the backend to work with the system as a whole and edited front end to work with it

--- src/main/java/com/revature/thevault/service/interfaces/AccountProfileInterface.java
+++ src/main/java/com/revature/thevault/service/interfaces/AccountProfileInterface.java
@@ -2,10 +2,12 @@ package com.revature.thevault.service.interfaces;
 
 import com.revature.thevault.presentation.model.request.AccountProfileRequest;
 import com.revature.thevault.presentation.model.request.ProfileCreateRequest;
+import com.revature.thevault.presentation.model.request.UpdateProfileRequest;
 import com.revature.thevault.presentation.model.response.AccountProfileResponse;
 import com.revature.thevault.presentation.model.response.builder.DeleteResponse;
 import com.revature.thevault.presentation.model.response.builder.GetResponse;
 import com.revature.thevault.presentation.model.response.builder.PostResponse;
+import com.revature.thevault.presentation.model.response.builder.PutResponse;
 import com.revature.thevault.repository.entity.LoginCredentialEntity;
 
 public interface AccountProfileInterface {
@@ -13,7 +15,7 @@ public interface AccountProfileInterface {
 
     PostResponse createProfile(ProfileCreateRequest profileCreateRequest);
 
-    PostResponse updateProfile(ProfileCreateRequest profileCreateRequest);
+    PutResponse updateProfile(UpdateProfileRequest updateProfileRequest);
 
     DeleteResponse deleteProfile(AccountProfileRequest accountProfileRequest);
 }
